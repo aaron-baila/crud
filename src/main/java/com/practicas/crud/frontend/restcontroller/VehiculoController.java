886d3b9435aaf0a072aae185555a7bf4ad06f0ed
Aarón Baila <75446506+aaron-baila@users.noreply.github.com>
2023-12-28 12:23:42 +0100

    Update VehiculoController.java

--- src/main/java/com/practicas/crud/frontend/restcontroller/VehiculoController.java
+++ src/main/java/com/practicas/crud/frontend/restcontroller/VehiculoController.java
@@ -3,9 +3,9 @@ package com.practicas.crud.frontend.restcontroller;
 import com.practicas.crud.backend.entity.Vehiculo;
 import com.practicas.crud.backend.service.VehiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -19,5 +19,23 @@ public class VehiculoController {
     @GetMapping
     public List<Vehiculo> getAll(){
         return  vehiculoService.getAll();
+    }
+    @PostMapping
+    public ResponseEntity<?> createVehiculo(@RequestBody Vehiculo vehiculo, UriComponentsBuilder direccion){
+
+        try {
+            Vehiculo nuevoVehiculo = vehiculoService.create(vehiculo);
+            Long id = nuevoVehiculo.getId();
+
+            return ResponseEntity
+                    .created(direccion.path("/vehiculos/{id}")
+                            .build(id))
+                    .build();
+
+        } catch (IllegalStateException e) {
+            //TODO crear nuestras propias excepciones
+            throw new RuntimeException(e);
+        }
+
     }
 }
